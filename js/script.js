46c8be6fdb0fa62e3564bbcdd92d0865afed07bd
anna <13santoscarolina@gmail.com>
2024-03-30 22:23:04 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//efeito do botão voltar ao Topo
+
+function Topo(){
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:"smooth"
+        }
+    )
+}
+
+
+
+//Validação de Login
+
+function Login(){
+    var logado  = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == "anna" && senha == "momotwice"){
+        window.location = "index.html";
+        logado = 1;
+    }
+
+    if(logado == 0){
+        alert("Negado !!! confira dados  ")
+    }
+}
+
+//Ativar alert no botão cadastrar
+
+function cadastro(){
+    alert("Cadastrado com sucesso!");
+    window.location.href = "index.html"
+}
